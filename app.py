2ac06d79bdd2d84d3d1ee515a2a7afdeb64c11e2
Leouch-d <121665124+Leouch-d@users.noreply.github.com>
2025-07-09 12:31:26 +0000

    Dodanie Dockerfile dla pewnego wdrożenia

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+# run.py
+# Ten plik służy tylko do uruchomienia naszej aplikacji.
+# Upewnij się, że jest on w głównym folderze projektu (nasz-budzet-backend).
+
 from nasz_budzet import create_app
 
+# Tworzymy instancję aplikacji za pomocą naszej fabryki
 app = create_app()
+
+if __name__ == '__main__':
+    # Uruchamiamy serwer deweloperski
+    app.run(host='0.0.0.0', port=5000, debug=True)
